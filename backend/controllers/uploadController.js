118f5551d8af793fa03ef09dc124225f752834e4
Yasmeen Awawdeh (Yasmin MSA) <131165262+yasameenmsa@users.noreply.github.com>
2023-10-14 00:34:03 +0300

    Update uploadController.js

--- backend/controllers/uploadController.js
+++ backend/controllers/uploadController.js
@@ -15,12 +15,20 @@ const upload = multer({
     storage: storage,
 });
 
+
 uploadController.post("/image", upload.single("image"), async (req, res) => {
     try {
-        return res.status(200).json("File uploded successfully");
+        // Check if the file was uploaded successfully
+        if (req.file) {
+            return res.status(200).json("File uploaded successfully");
+        } else {
+            return res.status(400).json("File upload failed");
+        }
     } catch (error) {
         console.error(error);
+        return res.status(500).json("Server error");
     }
 });
+
 
 module.exports = uploadController
